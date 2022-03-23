9ce03dd9e13cf53a36eff22acf7c8bef4893391a
fikriwado <fixwad.online@gmail.com>
2022-03-23 18:44:45 +0700

    use component

---
+++ src/pages/home/index.js
@@ -0,0 +1,14 @@
+const Home = () => {
+    return (
+        <div>
+            <h1>Hello Exercise Module 2 Session 1</h1>
+            <div style={{ paddingBottom: "25px" }}>
+                <input type="text" />
+                <button>Search</button>
+            </div>
+            <img src="https://media.giphy.com/media/Vh8pbGX3SGRwFDh3V0/source.gif" alt="" />
+        </div>
+    );
+}
+
+export default Home;
