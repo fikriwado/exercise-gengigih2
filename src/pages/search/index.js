ac4c9bec2bab0e7cd7c9335cbda569bda9d78137
fikriwado <fixwad.online@gmail.com>
2022-04-01 14:50:26 +0700

    functional component

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -1,45 +1,36 @@
+import { useState } from "react";
 import Gif from "../../components/Gif";
-import { Component } from "react";
 
-class Search extends Component {
-    state = {
-        gifs: [],
-        query: ''
-    };
-    
-    getGifs = async () => {
+const Search = () => {
+    const [gifs, setGifs] = useState([]);
+    const [query, setQuery] = useState('');
+
+    const getGifs = async () => {
         const gifs = await fetch(
-            'https://api.giphy.com/v1/gifs/search?api_key=' + process.env.REACT_APP_GIPHY_KEY + '&q=' + this.state.query + '&limit=12',
+            'https://api.giphy.com/v1/gifs/search?api_key=' + process.env.REACT_APP_GIPHY_KEY + '&q=' + query + '&limit=12',
         ).then((response) => response.json())
         .then(json => json.data)
 
-        this.setState({gifs});
+        setGifs(gifs);
     };
 
-    handleInput = e => this.setState({ query: e.target.value });
+    return (
+        <>
+            <input type="text" onChange={e => setQuery(e.target.value)} />
+            <button onClick={getGifs}>Search</button>
 
-    render() {
-        const { gifs } = this.state;
-        console.info(gifs);
-
-        return (
-            <>
-                <input type="text" onChange={this.handleInput} />
-                <button onClick={this.getGifs}>Search</button>
-
-                <div className="list-gif">
-                    {gifs.map((item) => {
-                        return (
-                            <div className="list-item" key={item.id}>
-                                <Gif url={item.images.fixed_width.url} title={item.title} />
-                                <p>{item.title}</p>
-                            </div>
-                        );
-                    })}
-                </div>
-            </> 
-        )
-    }
+            <div className="list-gif">
+                {gifs.map((item) => {
+                    return (
+                        <div className="list-item" key={item.id}>
+                            <Gif url={item.images.fixed_width.url} title={item.title} />
+                            <p>{item.title}</p>
+                        </div>
+                    );
+                })}
+            </div>
+        </> 
+    );
 }
 
 export default Search;
